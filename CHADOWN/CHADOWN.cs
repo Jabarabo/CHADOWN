921bed24d5414f76e074c62633e6e7b9129fa618
Sysroot <20374744+hochladen@users.noreply.github.com>
2021-03-12 14:33:04 -0600

    Code cleanup

--- CHADOWN/CHADOWN.cs
+++ CHADOWN/CHADOWN.cs
@@ -1,22 +1,19 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 namespace CHADOWN
 {
-    public class YTDLMethodClass //Module for Youtube-DL support
+    public static class YTDLMethodClass //Module for Youtube-DL support
     {
-        string YTDLtype;
-
-        public void YTDL_proper() //Main part of the YTDL module.
+        public static void YTDLmethod() //Main part of the YTDL module.
         {
             string[] YTDLarr = new string[5];
             YTDLarr[4] = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
             Console.Clear();
-            Console.WriteLine("Would you like the file to be Audio/Video or just audio?");
-            Console.WriteLine("");
+            Console.WriteLine("Would you like the file to be Audio/Video or just audio?\n");
             Console.WriteLine("(a) Audio/Video");
             Console.WriteLine("(b) Just Audio");
-            YTDLtype = Console.ReadLine();
+            string YTDLtype = Console.ReadLine();
             Console.Clear();
             if (YTDLtype == "b")
             {
@@ -36,41 +33,38 @@ namespace CHADOWN
             Console.WriteLine("Paste the URL of the Youtube video.");
             YTDLarr[3] = Console.ReadLine();
             Console.Clear();
-            string strCmdText;
-            strCmdText = "/C youtube-dl "+YTDLarr[0]+" --output "+YTDLarr[4]+@"\"+YTDLarr[2]+YTDLarr[1]+" "+YTDLarr[3];
+            string strCmdText = $@"/C youtube-dl ${YTDLarr[0]} --output ${YTDLarr[4]}\${YTDLarr[2] + YTDLarr[1]} ${YTDLarr[3]}";
             Process p = Process.Start("CMD.exe", strCmdText);
             p.WaitForExit();
         }
     }
-    public class FMPGMethodClass //Module for ffmpeg (BLOB) support
+    public static class FMPGMethodClass //Module for ffmpeg (BLOB) support
     {
-        string FMPGlink;
-        string FMPGvideoname;
-        public string FMPGLinkGet() //Asks the user for the link to the playlist and returns it.
+        public static void FMPGmethod() //Main part of the ffmpeg module
+        {
+            string FMPGlink = FMPGLinkGet();
+            string FMPGvideoname = FMPGNameGet();
+            string strCmdText = $@"/C ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i ${FMPGlink} ${Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)}\${FMPGvideoname}.mp4";
+            Process p = Process.Start("CMD.exe", strCmdText);
+            p.WaitForExit();
+        }
+        private static string FMPGLinkGet() //Asks the user for the link to the playlist and returns it.
         {
             Console.WriteLine("Please paste the link for the playlist (usually a .m3u8) file");
-            FMPGlink = Console.ReadLine();
+            string FMPGlink = Console.ReadLine();
             Console.Clear();
             return FMPGlink;
         }
-        public string FMPGNameGet() //Asks the user for what they want the video name to be and returns it.
+        private static string FMPGNameGet() //Asks the user for what they want the video name to be and returns it.
         {
             Console.WriteLine("What would you like to name the video (Don't include the filetype in the name)?");
-            FMPGvideoname = Console.ReadLine();
+            string FMPGvideoname = Console.ReadLine();
             Console.Clear();
             return FMPGvideoname;
-        }
-        public void FMPGmethod() //Main part of the ffmpeg module
-        {
-            string strCmdText;
-            strCmdText = "/C ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "+FMPGlink+" "+Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)+@"\"+FMPGvideoname+".mp4";
-            Process p = Process.Start("CMD.exe", strCmdText);
-            p.WaitForExit();
         }
     }
     class CHADOWN
     {
-        static bool Continue = true;
         static void TCol(string RText, string WText)
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -80,14 +74,13 @@ namespace CHADOWN
         }
         static void Main(string[] args)
         {
-            while (Continue)
+            while (true)
             {
                 Console.Write("Welcome to Jabs' ");
                 TCol("C", "heap ");
                 TCol("A", "ss ");
                 TCol("Down", "loader!\n");
-                Console.WriteLine("Now with 100% less shell scripting!");
-                Console.WriteLine("");
+                Console.WriteLine("Now with 100% less shell scripting!\n");
                 Console.WriteLine("Please select which type of video you wish to *procure*");
                 Console.WriteLine("New types will be added as new technologies are discovered.");
                 Console.WriteLine("---------------");
@@ -95,27 +88,19 @@ namespace CHADOWN
                 Console.WriteLine("B. BLOB/M3U8 Streams");
                 string ConInputType = Console.ReadLine(); //User selects which one they want.
                 Console.Clear();
-                if (ConInputType == "A") //Method calls for YTDL
+                if (ConInputType == "A") //Method call for YTDL
                 {
-                    YTDLMethodClass YTDLObj = new YTDLMethodClass();
-                    YTDLObj.YTDL_proper();
+                    YTDLMethodClass.YTDLmethod();
                 }
-                else if (ConInputType == "B") //Method calls for ffmpeg
+                else if (ConInputType == "B") //Method call for ffmpeg
                 {
-                    FMPGMethodClass FMPGObj = new FMPGMethodClass();
-                    FMPGObj.FMPGLinkGet();
-                    FMPGObj.FMPGNameGet();
-                    FMPGObj.FMPGmethod();
+                    FMPGMethodClass.FMPGmethod();
                 }
-                else
-                {
-                    continue;
-                }
+                else continue; //Repeat if user selects invalid option
                 Console.WriteLine("Continue? (y/n)");
-                string WeGoingAgain = Console.ReadLine(); //The user chooses if they need to do another video
-                if (WeGoingAgain == "n")
+                if (Console.ReadLine() == "n") //The user chooses if they need to do another video
                 {
-                    Continue = false; //Once set to false script will kill. Defaults to true.
+                    return; //Kill script by returning from main.
                 }
                 Console.Clear();
             }
